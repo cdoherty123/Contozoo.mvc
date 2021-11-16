c04ec65debc195ff1016aee1cecb76e13699ed1e
Courtney Doherty <cdoherty@microsoft.com>
2021-11-16 16:48:09 +0000

    Added connection to API

--- Contozoo.mvc/Controllers/HomeController.cs
+++ Contozoo.mvc/Controllers/HomeController.cs
@@ -2,28 +2,49 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+
 
 namespace Contozoo.mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        Uri baseAddress = new Uri("https://contozooapi.azurewebsites.net");
+        HttpClient client;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
         }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Animals()
+        {
+            List<AnimalModel> animals = new List<AnimalModel>();
+            HttpResponseMessage response = client.GetAsync(baseAddress + "/api/Animals").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;
+                animals = JsonConvert.DeserializeObject<List<AnimalModel>>(data);
+            }
+            return View(animals);
+        }
+            public IActionResult Privacy()
         {
             return View();
         }
---
+++ Contozoo.mvc/Models/AnimalModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contozoo.mvc.Models
+
+{
+    public class AnimalModel
+    {
+        public int AnimalId { get; set; }
+        public int CAI { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime LunchHour { get; set; }
+        public string Notes { get; set; }
+    }
+}
